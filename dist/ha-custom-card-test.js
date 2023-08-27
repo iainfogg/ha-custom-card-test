140c3b506f098213c9dce2d010cb5b5617110b6d
Iain F <7030201+iainfogg@users.noreply.github.com>
2023-08-27 22:42:58 +0100

    Add missed card type definition

--- dist/ha-custom-card-test.js
+++ dist/ha-custom-card-test.js
@@ -39,7 +39,7 @@ class HaCustomTestCard extends HTMLElement {
     }
   }
   
-  customElements.define("", ContentCardExample);
+  customElements.define("ha-custom-card-test", ContentCardExample);
   window.customCards.push({
     type: 'ha-custom-card-test',
     name: 'Iain Test Card',
